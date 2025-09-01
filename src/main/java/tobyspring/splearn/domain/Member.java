14156a336db9ec4565ce8f718666a16425cfc6f8
Jealyang <jaelyangchoi@gmail.com>
2025-09-01 22:37:47 +0900

    feat: Member 도메인 로직 개발

--- src/main/java/tobyspring/splearn/domain/Member.java
+++ src/main/java/tobyspring/splearn/domain/Member.java
@@ -1,11 +1,16 @@
 package tobyspring.splearn.domain;
 
+import static org.springframework.util.Assert.*;
+
+import java.util.Objects;
+
 import lombok.Getter;
-import lombok.NonNull;
+import lombok.ToString;
 
 @Getter
+@ToString
 public class Member {
-	@NonNull
+
 	private String email;
 
 	private String nickname;
@@ -15,9 +20,22 @@ public class Member {
 	private MemberStatus status;
 
 	public Member(String email, String nickname, String passwordHash) {
-		this.email = email;
-		this.nickname = nickname;
-		this.passwordHash = passwordHash;
+		this.email = Objects.requireNonNull(email);
+		this.nickname = Objects.requireNonNull(nickname);
+		this.passwordHash = Objects.requireNonNull(passwordHash);
+
 		this.status = MemberStatus.PENDING;
+	}
+
+	public void activate() {
+		state(status == MemberStatus.PENDING, "PENDING 상태가 아닙니다.");
+
+		this.status = MemberStatus.ACTIVE;
+	}
+
+	public void deactivate() {
+		state(status == MemberStatus.ACTIVE, "ACTIVE 상태가 아닙니다.");
+
+		this.status = MemberStatus.DEACTIVATED;
 	}
 }
--- src/test/java/tobyspring/splearn/domain/MemberTest.java
+++ src/test/java/tobyspring/splearn/domain/MemberTest.java
@@ -12,4 +12,50 @@ class MemberTest {
 
 		assertThat(member.getStatus()).isEqualTo(MemberStatus.PENDING);
 	}
+
+	@Test
+	void constructorNullCheck() {
+		assertThatThrownBy(() -> new Member(null, "jaeryang", "secret"))
+			.isInstanceOf(NullPointerException.class);
+	}
+
+	@Test
+	void activate() {
+		var member = new Member("cjl2076@naver.com", "jaeryang", "secret");
+
+		member.activate();
+
+		assertThat(member.getStatus()).isEqualTo(MemberStatus.ACTIVE);
+	}
+
+	@Test
+	void activateFail() {
+		var member = new Member("cjl2076@naver.com", "jaeryang", "secret");
+
+		member.activate();
+
+		assertThatThrownBy(member::activate).isInstanceOf(IllegalStateException.class);
+	}
+
+	@Test
+	void deactivate() {
+		var member = new Member("cjl2076@naver.com", "jaeryang", "secret");
+		member.activate();
+
+		member.deactivate();
+
+		assertThat(member.getStatus()).isEqualTo(MemberStatus.DEACTIVATED);
+	}
+
+	@Test
+	void deactivateFail() {
+		var member = new Member("cjl2076@naver.com", "jaeryang", "secret");
+
+		assertThatThrownBy(member::deactivate).isInstanceOf(IllegalStateException.class);
+
+		member.activate();
+		member.deactivate();
+
+		assertThatThrownBy(member::deactivate).isInstanceOf(IllegalStateException.class);
+	}
 }
